f2431e64804027353f6e31b9ad57199c01b3e603
daryll <darylldigo@gmail.com>
2018-11-05 15:06:59 +0800

    ako budoy

--- app/webroot/js/common.js
+++ app/webroot/js/common.js
@@ -1,27 +1,6 @@
 $(function() {
-  handleFeedbackModal();
   handleClickMenu();
 });
-
-
-// SHOW SUGGESTIONS AND FEEDBACK MODAL
-function handleFeedbackModal() {
-  $('html').delegate('.feedback-results', 'click',function() {
-    $('.modal').fadeIn(300);
-    $('.modal-course-title').text($(this).parents('.admin-item').find('.admin-name').text());
-    $('.modal-course-department').text($(this).parents('.admin-item').find('.admin-department').text());
-    $('.modal-feedback-form').attr('id',$(this).parents('.admin-item').data('category'));
-  }); 
-
-  $('.close-modal').on('click', function() {
-    $('.modal').fadeOut(300);
-    $('.modal-course-title, .modal-course-department').text('');
-    $('.modal-feedback-form').attr('id','');
-    $('.modal-feedback-input, .modal-feedback-textarea').val('');
-    $('.modal-feedback-selectbox').val(5)
-  })
-}
-
 
 // DISPLAY THE COURSES OR ADMINS 
 function handleShowListOfCourses( department, departmentAdmin ) {
@@ -32,23 +11,12 @@ function handleShowListOfCourses( department, departmentAdmin ) {
     dataType: 'json',
     success: function(response) {
       response[department][0][departmentAdmin].forEach((value,key) => {
-        if (departmentAdmin == 'dean') {
-          $('#department-admin').text(value['department'] + " Dean " + value['facultyName']);
-        } else {
-          $('#department-admin').text(value['department'] + " " + value['facultyName']);
-        }
-        $('#admin-list').append(
-          `<li class="admin-item" data-category="`+ department + '-' + departmentAdmin +`">
-            <div class="admin-description">
-              <span class="admin-name">`+ value['facultyName'] + `</span>
-              <span class="admin-department">`+ value['department'] +`</span>
-              <p>`+ value['description'] +`</p>
-            </div>
-            <a class="feedback-results">
-              <span>Comments and Suggestions (<small class="admin-suggestions-rate">`+ value['numbersOfFeedback'] +`</small>)</span>
-            </a>
-        </li>`
-        )
+        setTimeout(function(){
+          $('.admin-course-title').text(value['facultyName']);
+          $('.admin-course-department').text(value['department']);
+          $('.admin-course-description p').html(value['description']);
+          $('.admin-feedback-form').attr('id',department +'-'+departmentAdmin);
+        },300)
       });
     }
   })
@@ -59,12 +27,17 @@ function handleClickMenu() {
   $('.navigation-link').on('click', function() {
     let getDataCategory = $(this).data('category');
     let splitDepartment = getDataCategory.split('-');
-    $('#admin-page').fadeIn(300);
+    $('.admin').fadeOut(300);
+    $('.admin-feedback-selectbox').val(5);
+    $('.admin-feedback-input, .admin-feedback-textarea').val('');
+
+    setTimeout(function(){
+      $('.admin').fadeIn(300);
+    },100)
+
     $('#welcome-message').fadeOut();
     let getDepartmentAdmin = splitDepartment[1];
     let getDepartment = splitDepartment[0];
-  
-    $('#admin-list, #department-admin').html('');
     handleShowListOfCourses( getDepartment, getDepartmentAdmin ); 
   });
 }
